17e658040db1c77feeb7c9c2832ba89e84c1586d
Alejandro <alejandrobarr.sosa@hotmail.com>
2025-06-08 17:29:49 -0500

    landing

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function sumar(a, b) {
+    if (a > b) {
+        return a - b;
+    } else {
+        return a + b;
+    }
+}
+
+console.log(sumar(2, 3));
+console.log(sumar(4, 3));
+console.log(sumar(6, 3));
+console.log(sumar(8, 3));
+console.log(sumar(1, 3));
